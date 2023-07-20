37c3ed94b73c9643132a45b6de76c68579501286
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-07-20 16:23:03 -0600

    Create spring boot MVC project, add JPA project dependency

---
+++ FaceBakawk/src/main/java/com/skilldistillery/facebakawk/FaceBakawkApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.facebakawk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceBakawkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceBakawkApplication.class, args);
+	}
+
+}
