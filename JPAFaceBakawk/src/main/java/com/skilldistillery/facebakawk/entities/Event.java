40a32599b984b5f140e8c60322de3540ba69d627
Jay Perkins <jul3jb@gmail.com>
2023-07-21 13:38:19 -0700

    added Event entity

--- JPAFaceBakawk/src/main/java/com/skilldistillery/facebakawk/entities/Event.java
+++ JPAFaceBakawk/src/main/java/com/skilldistillery/facebakawk/entities/Event.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.facebakawk.entities;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,5 +15,145 @@ public class Event {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String title;
+	@Column(name="start_time")
+	private LocalDateTime startTime;
+	@Column(name="end_time")
+	private LocalDateTime endTime;
+	@Column(name="event_date")
+	private LocalDateTime eventDate;
+	private String description;
+	@Column(name= "picture_url")
+	private String pictureURL;
+	@Column(name="create_date")
+	private LocalDateTime createDate;
+	@Column(name="last_update")
+	private LocalDateTime lastUpdate;
+	
+	
+	public Event() {
+		
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+
+	public LocalDateTime getEventDate() {
+		return eventDate;
+	}
+
+
+	public void setEventDate(LocalDateTime eventDate) {
+		this.eventDate = eventDate;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getPictureURL() {
+		return pictureURL;
+	}
+
+
+	public void setPictureURL(String pictureURL) {
+		this.pictureURL = pictureURL;
+	}
+
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+
+	public LocalDateTime getLastUpdate() {
+		return lastUpdate;
+	}
+
+
+	public void setLastUpdate(LocalDateTime lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		return id == other.id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", title=" + title + ", startTime=" + startTime + ", endTime=" + endTime
+				+ ", eventDate=" + eventDate + ", description=" + description + ", pictureURL=" + pictureURL
+				+ ", createDate=" + createDate + ", lastUpdate=" + lastUpdate + "]";
+	}
+	
+	
 
 }
+
