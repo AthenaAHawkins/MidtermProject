3b85d01b13364950d1bc0d3bb499f40cf06c01ca
necron57 <jacoba1995@yahoo.com>
2023-07-21 14:47:47 -0600

    address test passed

---
+++ JPAFaceBakawk/src/main/java/com/skilldistillery/facebakawk/entities/Address.java
@@ -0,0 +1,104 @@
+package com.skilldistillery.facebakawk.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String city;
+
+	private String state;
+
+	private String country;
+
+	private String zipcode;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+
+	public Address() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZipCode() {
+		return zipcode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipcode = zipCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", city=" + city + ", state=" + state + ", country=" + country + ", zipcode="
+				+ zipcode + ", phoneNumber=" + phoneNumber + "]";
+	}
+
+}
