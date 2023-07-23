01c90f4b68a5707b489ed93b17fa9a95539ff1e0
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-07-23 08:21:34 -0600

    chickenDAO added

---
+++ FaceBakawk/src/main/java/com/skilldistillery/facebakawk/data/ChickkenDAO.java
@@ -0,0 +1,21 @@
+package com.skilldistillery.facebakawk.data;
+
+import java.util.List;
+
+import com.skilldistillery.facebakawk.entities.Chicken;
+
+public interface ChickkenDAO  {
+	
+	List<Chicken> findAll();
+
+	List<Chicken> searchByKeyword(String searchTerm);
+
+	Chicken findUserById(int userId);
+
+	Chicken create(Chicken chicken);
+
+	Chicken updateChicken(int userId, Chicken chicken);
+
+	boolean deleteById(int userId);
+
+}
